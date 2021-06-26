272f2a7cc78a8efc597b5c7be882b2e117649723
MdTasib <ohidulalam442@gmail.com>
2021-06-26 17:00:27 +0600

    context

--- src/Components/Context/Content.js
+++ src/Components/Context/Content.js
@@ -1,21 +1,25 @@
 import Counter from './Counter';
 import MouseHoverCount from './MouseHoverCount';
 import ThemeContext from '../Context/themeContext';
+import React, { useContext } from 'react';
 
 export default function Content() {
+    const context = useContext(ThemeContext);
+    const { theme, switchTheme } = context;
+
     return (
         <div>
             <h2>This is content</h2>
             <Counter>
                 {
-                    (counter, incrementCount) => {
-                        return <ThemeContext.Consumer>{({ theme, switchTheme }) => <MouseHoverCount
+                    (counter, incrementCount) => (
+                        <MouseHoverCount
                             theme={theme}
                             count={counter}
                             incrementCount={incrementCount}
                             switchTheme={switchTheme}
-                        />}</ThemeContext.Consumer>
-                    }
+                        />
+                    )
                 }
             </Counter>
         </div>
